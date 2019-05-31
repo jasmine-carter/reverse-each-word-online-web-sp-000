25669485eee41c7b70846c923bed951c68df259b
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-31 00:38:14 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(string)
+  array = string.split("\s")
+  return_value = []
+  array.each do |word|
+    #word.reverse
+    return_value.push(word.reverse)
+  end
+  return return_value.join("\s")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+  end
+end
