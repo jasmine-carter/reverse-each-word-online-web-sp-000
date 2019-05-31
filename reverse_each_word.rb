a607381d5d195e3d3fcea40cb5136028a1bff1cb
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-31 01:07:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,8 @@ end
 
 def reverse_each_word(string)
   array = string.split(" ")
-new_array = array.collect do |word| word.reverse
+  new_array = []
+  new_array = array.collect {|word| word.reverse}
   new_array.join(" ")
   end
 end
